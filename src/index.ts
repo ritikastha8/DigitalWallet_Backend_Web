81fa58c8d7bc6c871ceeee15bc396d82ec5ef460
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-20 16:57:00 +0545

    Updated index file

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,19 @@ import bodyParser from "body-parser";
 import { connectDatabase } from "./database/mongodb";
 import { PORT } from "./config";
 import authRoutes from "./routes/auth.route";
-
+import cors from 'cors';
 const app: Application = express();
+
+
+let corsOptions = {
+    origin: ["http://localhost:3000","http://localhost:3003"],
+    // list of domains allowed to access the server
+    // frontend domain/url
+    optionsSuccessStatus: 200,
+    credentials: true,
+}
+// origin: "*" , // allow all domains
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
